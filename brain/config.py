cce7fa42c2bb5d8011dabba2d65f95a8a26d8538
danchang11 <cssource@livemail.tw>
2021-07-13 15:31:27 +0800

    init fork

--- brain/config.py
+++ brain/config.py
@@ -7,7 +7,7 @@ KGS = {
     'HowNet': os.path.join(FILE_DIR_PATH, 'kgs/HowNet.spo'),
     'CnDbpedia': os.path.join(FILE_DIR_PATH, 'kgs/CnDbpedia.spo'),
     'Medical': os.path.join(FILE_DIR_PATH, 'kgs/Medical.spo'),
-    'concate': os.path.join(FILE_DIR_PATH), 'kgs/concate.spo'
+    'concate': os.path.join(FILE_DIR_PATH), 'kgs/concate.spo')
 }
 
 MAX_ENTITIES = 5
